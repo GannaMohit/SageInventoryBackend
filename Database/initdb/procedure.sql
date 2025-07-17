a2bde0516f79302445b14f0c4e18d5fe7ed49e5d
Powichroski, Philip Alberto <ppowichroski3@gatech.edu>
2025-07-17 12:00:07 -0400

    database procedures
    add procedures

---
+++ Database/initdb/procedure.sql
@@ -0,0 +1,61 @@
+
+USE `schema`;
+
+-- User Procedures
+
+--add product to inventory procedure
+CREATE PROCEDURE add_product_to_inventory(IN p_name VARCHAR(100), IN dept VARCHAR(50), IN price DECIMAL(10, 2), IN quantity INT)
+BEGIN
+    INSERT INTO Product (P_name, Dept, price, Quantity) VALUES (p_name, dept, price, quantity);
+END;
+
+
+
+-- change product price
+CREATE PROCEDURE change_product_price(IN p_id INT, IN new_price DECIMAL(10, 2))
+BEGIN
+    UPDATE Product SET price = new_price WHERE P_ID = p_id;
+END;
+
+--change product quantity quantity
+CREATE PROCEDURE change_product_quantity(IN p_id INT, IN by_amount INT)
+BEGIN
+    UPDATE Product SET Quantity = Quantity + by_amount WHERE P_ID = p_id;
+END;
+
+-- remove prodcut inventory
+CREATE PROCEDURE remove_product_from_inventory(IN p_id INT)
+BEGIN
+    update Product SET Quantity = 0 WHERE P_ID = p_id;
+END;
+
+
+-- get a product by id
+CREATE PROCEDURE get_product_by_id(IN p_id INT)
+BEGIN
+    SELECT * FROM Product WHERE P_ID = p_id;
+END;
+
+-- get all products
+CREATE PROCEDURE get_all_products()
+BEGIN
+    SELECT * FROM Product;
+END;
+
+--set prodcut quantity
+CREATE PROCEDURE set_product_quantity(IN p_id INT, IN new_quantity INT)
+BEGIN
+    UPDATE Product SET Quantity = new_quantity WHERE P_ID = p_id;
+END;
+
+--create invoice
+CREATE PROCEDURE create_invoice(IN date DATE, IN time TIME, IN status ENUM('paid', 'pending', 'canceled'))
+BEGIN
+    INSERT INTO Invoice (Date, Time, Status) VALUES (date, time, status);
+END;
+
+--add product to invoice
+CREATE PROCEDURE add_product_to_invoice(IN p_id INT, IN i_id INT, IN quantity INT)
+BEGIN
+    INSERT INTO Product_Invoice (P_ID, I_ID, Quantity) VALUES (p_id, i_id, quantity);
+END;
--- Database/initdb/schema.sql
+++ Database/initdb/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE Product (
     P_ID INT AUTO_INCREMENT PRIMARY KEY,
     P_name VARCHAR(100) NOT NULL,
     Dept VARCHAR(50),
+    price DECIMAL(10, 2) NOT NULL,
     Quantity INT DEFAULT 0
 );
 
@@ -21,3 +22,15 @@ CREATE TABLE Invoice (
     Time TIME NOT NULL,
     Status ENUM('paid', 'pending', 'canceled') NOT NULL
 );
+
+--products in invoice table
+create TABLE Product_Invoice (
+    PI_ID INT AUTO_INCREMENT PRIMARY KEY,
+    P_ID INT NOT NULL,
+    I_ID INT NOT NULL,
+    Quantity INT DEFAULT 1,
+    FOREIGN KEY (P_ID) REFERENCES Product(P_ID),
+    FOREIGN KEY (I_ID) REFERENCES Invoice(I_ID)
+);
+
+
